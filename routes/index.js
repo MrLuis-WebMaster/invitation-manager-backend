8270ad881885649037db48107cee4dad2808593a
Luis Martinez <mr.luiswebmaster@gmail.com>
2023-06-16 10:16:16 -0500

    feat: :sparkles: index for routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const usersRouter = require('./users.route');
 const guestsRouter = require('./guests.route');
 const notificationRouter = require('./notifications.route');
 const whatsappRouter = require('./whatsapp.route');
+const stripeRouter = require('./stripe.route');
+const eventRouter = require('./event.route');
+const eventTemplateRouter = require('./eventTemplate.route');
+const expenseRouter = require('./expense.route');
+const taskRouter = require('./task.route');
+const templatesRouter = require('./templates.route');
+const providersRouter = require('./provider.route');
 
 const router = express.Router();
 
@@ -16,5 +23,12 @@ router.use('/users', usersRouter);
 router.use('/guests', guestsRouter);
 router.use('/notifications', notificationRouter);
 router.use('/whatsapp', whatsappRouter);
+router.use('/stripe', stripeRouter);
+router.use('/event', eventRouter);
+router.use('/event/template', eventTemplateRouter);
+router.use('/expense', expenseRouter);
+router.use('/task', taskRouter);
+router.use('/templates', templatesRouter);
+router.use('/providers', providersRouter);
 
 module.exports = router;
